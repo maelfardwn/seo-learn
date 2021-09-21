7b8b7dc47f4a81ba87a3fadb2e370df6d8b8f658
maelfardwn <maelfa011@gmail.com>
2021-09-22 02:37:27 +0700

    commit3

--- src/App.js
+++ src/App.js
@@ -22,9 +22,20 @@ function App() {
 
   return ( 
   <BrowserRouter>
-    <Helmet>
-    <title>Homepage index</title>
-    <meta name="description" content="This is the homepage index"/>
+       <Helmet>
+       <title>this is homepage index</title>
+    <meta name="image" content='https://assesmenttestridwan.herokuapp.com/assets/london.jpg'/>
+    <meta name="description" content="Description this Home"/>
+    
+    <meta property="og:title" content='opengraph test homepage'/>
+    <meta property="og:description" content='This is homepage using opengraph for testing'/>
+    <meta property="og:description" content="Description this"/>
+    <meta property="og:image" content='https://assesmenttestridwan.herokuapp.com/assets/london.jpg'/>
+    <meta property="og:url" content='https://assesmenttestridwan.herokuapp.com/'/>
+    
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:image:alt" content='London Pict With Twitter image alt'/> 
+    <meta property="twitter:description" content="Description this"/>
     </Helmet>
     <Switch>
     <Route exact path='/about' component = { About } />
